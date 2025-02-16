3498aff3c2735f12cc3739ab3202550b194c28ea
filip-ba <bata.f@email.cz>
2025-02-16 23:01:19 +0100

    rename pytorch tab, fix icon path

--- fruitvegnet/main_window.py
+++ fruitvegnet/main_window.py
@@ -10,8 +10,8 @@ class MainWindow(QMainWindow):
         self._create_ui()
         
     def _create_ui(self):
-        project_dir = os.path.dirname(os.path.dirname(__file__))
-        icon_path = os.path.join(project_dir, "icon.ico")
+        project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        icon_path = os.path.join(project_root, "assets", "icon.ico")
         self.setWindowIcon(QIcon(icon_path))
         self.setWindowTitle("FruitVegNet")
         self.setGeometry(50, 50, 1200, 920)
@@ -19,9 +19,8 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(main_widget)
         main_layout = QVBoxLayout(main_widget)
         self.tab_widget = QTabWidget(self)
-        # PyTorch and Keras Tab
-        pytorch_tab = MainWidget()
-        self.tab_widget.addTab(pytorch_tab, "Simple CNN Model")
+        simple_cnn_model_tab = MainWidget()
+        self.tab_widget.addTab(simple_cnn_model_tab, "Simple CNN Model")
         main_layout.addWidget(self.tab_widget)
         # MenuBar
         menu_bar = self.menuBar()
@@ -31,8 +30,8 @@ class MainWindow(QMainWindow):
         self.load_action = QAction("Load Model", self)
         file_menu.addAction(self.save_action)
         file_menu.addAction(self.load_action)
-        self.save_action.triggered.connect(pytorch_tab.save_model)
-        self.load_action.triggered.connect(pytorch_tab.load_model)
+        self.save_action.triggered.connect(simple_cnn_model_tab.save_model)
+        self.load_action.triggered.connect(simple_cnn_model_tab.load_model)
         file_menu.addSeparator()
         # Quit action
         self.quit_action = QAction("Quit", self)
