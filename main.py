0a20223fd04379160f79c3b9706fd15a0977f7cc
filip-ba <bata.f@email.cz>
2025-03-03 15:36:48 +0100

    remove set_palette.py

--- main.py
+++ main.py
@@ -2,12 +2,8 @@ import sys
 from PyQt6.QtWidgets import QApplication
 from widgets.main_window import MainWindow
 
-from utils.set_palette import palette
-
 def main():
     app = QApplication(sys.argv)
-    app.setStyle("Fusion")    
-    app.setPalette(palette)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
