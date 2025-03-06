7e0dda58aadac5c8d743a8bb0a0fb118f0b02c70
filip-ba <bata.f@email.cz>
2025-03-06 16:46:13 +0100

    add styles.css

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import sys
 from PyQt6.QtWidgets import QApplication
 from widgets.main_window import MainWindow
 
+def load_stylesheet(path):
+    with open(path, "r") as f:
+        return f.read()
+
 def main():
     app = QApplication(sys.argv)
+    app.setStyleSheet(load_stylesheet("utils/styles.css"))
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
