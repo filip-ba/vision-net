0272370542a1ec3903dc57c24bac12d4fa2adb18
Filip <bata.f@email.cz>
2025-07-07 21:47:47 +0200

    remove dataset info box from model_tab

--- src/ui/widgets/model_info_widget.py
+++ src/ui/widgets/model_info_widget.py
@@ -24,15 +24,9 @@ class ModelInfoWidget(QWidget):
         self.model_status_label.setObjectName("ModelStatus")
         self.model_status_label.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse | Qt.TextInteractionFlag.TextSelectableByKeyboard)
 
-        self.dataset_status_label = QLabel("No dataset found")
-        self.dataset_status_label.setObjectName("ModelStatus")
-        self.dataset_status_label.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse | Qt.TextInteractionFlag.TextSelectableByKeyboard)
-
         layout.addWidget(self.model_file_label)
         layout.addWidget(create_separator("horizontal"))
         layout.addWidget(self.model_status_label)
-        layout.addWidget(create_separator("horizontal"))
-        layout.addWidget(self.dataset_status_label)
 
     def set_model_file(self, file_name, color=None):
         if color is None:
@@ -46,20 +40,11 @@ class ModelInfoWidget(QWidget):
             self.model_status_label.setStyleSheet("") 
         else:
             self.model_status_label.setStyleSheet(f"color: {color};")
-
-    def set_dataset_status(self, status, color=None):
-        self.dataset_status_label.setText(status)
-        if color is None:
-            self.dataset_status_label.setStyleSheet("")
-        else:
-            self.dataset_status_label.setStyleSheet(f"color: {color};")
       
     def get_project_root(self):
         if getattr(sys, 'frozen', False):
-            # Executable
             return sys._MEIPASS
         else:
-            # IDE
             current_file_path = os.path.abspath(__file__)
             widgets_dir = os.path.dirname(current_file_path)
             ui_dir = os.path.dirname(widgets_dir)
